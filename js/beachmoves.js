93bd0a5463a786d9bab127985b96a085a36adde9
Billy <williamlaurain@gmail.com>
2016-05-09 20:20:09 -0400

    back to mobile display

--- js/beachmoves.js
+++ js/beachmoves.js
@@ -15,6 +15,7 @@ var GAME_HEIGHT = 414;
 var renderer = PIXI.autoDetectRenderer(GAME_WIDTH, GAME_HEIGHT, rendererOptions); 
 var tileCorner = 10;
 var tileHeight, tileWidth;
+var isHorizontal;
 renderer.backgroundColor = 0xff0000;
 stage.interactive = true;
 
@@ -144,54 +145,54 @@ function resize() {
 //MAYBE DONT SCALE AT ALL? SMALLER THAN MOBILE WILL NEED SMALLER BOXES (worth it?)
 	rotateHorizontal();
 
-	// if(window.innerWidth > GAME_WIDTH && window.innerHeight > GAME_HEIGHT)
-	// { //Larger than needs be, desktop mode
-	// 	renderer.resize(GAME_WIDTH, GAME_HEIGHT);
-	// 	renderer.view.style.position = "relative";
+	if(window.innerWidth > GAME_WIDTH && window.innerHeight > GAME_HEIGHT)
+	{ //Larger than needs be, desktop mode
+		renderer.resize(GAME_WIDTH, GAME_HEIGHT);
+		renderer.view.style.position = "relative";
 
-	// 	var centerX = Math.ceil((window.innerWidth / 2.0) - (GAME_WIDTH / 2.0));
-	// 	var centerY = Math.ceil((window.innerHeight / 2.0) - (GAME_HEIGHT / 2.0));
+		var centerX = Math.ceil((window.innerWidth / 2.0) - (GAME_WIDTH / 2.0));
+		var centerY = Math.ceil((window.innerHeight / 2.0) - (GAME_HEIGHT / 2.0));
 
-	// 	renderer.view.style.top = centerY + "px";
-	// 	renderer.view.style.left = centerX + "px";
-	// }
-	// else
-	// {  //mobile/small
-	// 	renderer.view.style.position = "absolute";
-	// 	renderer.view.style.top = "0px";
-	// 	renderer.view.style.left = "0px";
+		renderer.view.style.top = centerY + "px";
+		renderer.view.style.left = centerX + "px";
+	}
+	else
+	{  //mobile/small
+		renderer.view.style.position = "absolute";
+		renderer.view.style.top = "0px";
+		renderer.view.style.left = "0px";
 
-	// 	 if(window.innerWidth > window.innerHeight)
-	// 	 { //horizontal
-	// 	  // Determine which screen dimension is most constrained
-	// 	  ratio = Math.min(window.innerWidth/GAME_WIDTH,
-	// 	                   window.innerHeight/GAME_HEIGHT);
+		 if(window.innerWidth > window.innerHeight)
+		 { //horizontal
+		  // Determine which screen dimension is most constrained
+		  ratio = Math.min(window.innerWidth/GAME_WIDTH,
+		                   window.innerHeight/GAME_HEIGHT);
 		 
-	// 	  // Scale the view appropriately to fill that dimension
-	// 	  if(ratio < 1) stage.scale.x = stage.scale.y = ratio;
-	// 	  else stage.scale.x = stage.scale.y = 1;
+		  // Scale the view appropriately to fill that dimension
+		  if(ratio < 1) stage.scale.x = stage.scale.y = ratio;
+		  else stage.scale.x = stage.scale.y = 1;
 		 
-	// 	  // Update the renderer dimensions
-	// 	  renderer.resize(Math.min(Math.ceil(GAME_WIDTH * ratio), GAME_WIDTH),
-	// 	                  Math.min(Math.ceil(GAME_HEIGHT * ratio), GAME_HEIGHT));
-	// 	}
-	// 	else
-	// 	{ //vertical
-	// 	  ratio = Math.min(window.innerWidth/GAME_HEIGHT,
-	// 	                   window.innerHeight/GAME_WIDTH);
+		  // Update the renderer dimensions
+		  renderer.resize(Math.min(Math.ceil(GAME_WIDTH * ratio), GAME_WIDTH),
+		                  Math.min(Math.ceil(GAME_HEIGHT * ratio), GAME_HEIGHT));
+		}
+		else
+		{ //vertical
+		  ratio = Math.min(window.innerWidth/GAME_HEIGHT,
+		                   window.innerHeight/GAME_WIDTH);
 		  
-	// 	  // Scale the view appropriately to fill that dimension
-	// 	  if(ratio < 1) stage.scale.x = stage.scale.y = ratio;
-	// 	  else stage.scale.x = stage.scale.y = 1;
+		  // Scale the view appropriately to fill that dimension
+		  if(ratio < 1) stage.scale.x = stage.scale.y = ratio;
+		  else stage.scale.x = stage.scale.y = 1;
 
-	// 	  rotateVertical();
-	// 	  // Update the renderer dimensions
-	// 	  renderer.resize(Math.min(Math.ceil(GAME_HEIGHT * ratio), GAME_HEIGHT),
-	// 	                  Math.min(Math.ceil(GAME_WIDTH * ratio), GAME_WIDTH));
-	// 	}
-	// }
+		  rotateVertical();
+		  // Update the renderer dimensions
+		  renderer.resize(Math.min(Math.ceil(GAME_HEIGHT * ratio), GAME_HEIGHT),
+		                  Math.min(Math.ceil(GAME_WIDTH * ratio), GAME_WIDTH));
+		}
+	}
 
- renderer.resize(window.innerWidth,window.innerHeight);
+ // renderer.resize(window.innerWidth,window.innerHeight);
  renderer.render(stage);
 }
 
