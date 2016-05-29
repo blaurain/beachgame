9ac0797d0b3a8e51f60aa33c111d19a253564beb
Billy <williamlaurain@gmail.com>
2016-05-29 00:46:07 -0400

    tilemap refactored

--- js/tileMaps.js
+++ js/tileMaps.js
@@ -1,6 +1,13 @@
-var grid1 = [
+var GAME = GAME || {};
+
+GAME.TileMap = function() {
+
+}
+GAME.TileMap.constructor = GAME.TileMap;
+
+GAME.TileMap.grid1 = [
   [1,0,0,0,2,0],
   [0,0,3,0,0,0],
   [0,0,0,0,0,0],
   [1,2,0,0,0,3]
-];
+]
